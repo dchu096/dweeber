5b6797846dca14668e8b5311f879200bb337cd59
daniel chu <danielchu096@gmail.com>
2020-12-24 17:42:52 +1300

    Add files via upload

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,1 +1,17 @@
+const { ErelaClient, Utils } = require("erela.js");
+const { nodes } = require("../../botconfig.json")
 
+module.exports = bot => {
+    console.log(`${bot.user.username} is online`);
+
+    bot.levels = new Map()
+        .set("none", 0.0)
+        .set("low", 0.10)
+        .set("medium", 0.15)
+        .set("high", 0.25);
+
+    let activities = [ `Memu Discord | ^2`, `DM for mods`], i = 0;
+
+    setInterval(() => bot.user.setActivity(`${activities[i++ % activities.length]}`, { type: "WATCHING" }), 15000)
+
+};
