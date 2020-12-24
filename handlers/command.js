24d7d2a7b4a87f2624d31129d84ef8b782c1103c
daniel chu <danielchu096@gmail.com>
2020-12-24 17:45:03 +1300

    Add files via upload

--- handlers/command.js
+++ handlers/command.js
@@ -1,1 +1,13 @@
+const { readdirSync } = require("fs")
 
+module.exports = (bot) => {
+    const load = dirs => {
+        const commands = readdirSync(`./commands/${dirs}/`).filter(d => d.endsWith('.js'));
+        for (let file of commands) {
+            let pull = require(`../commands/${dirs}/${file}`);
+            bot.commands.set(pull.config.name, pull);
+            if (pull.config.aliases) pull.config.aliases.forEach(a => bot.aliases.set(a, pull.config.name));
+          };
+        };
+        ["info", "moderation", "Memu", "other", "admins", "roles", "owner", "selfroles"].forEach(x => load(x));
+};
