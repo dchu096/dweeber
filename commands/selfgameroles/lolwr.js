d2010641cfb67be221058d1dd7a96c734c9ff340
daniel chu <danielchu096@gmail.com>
2021-01-02 08:54:50 +1300

    Add files via upload

--- commands/selfgameroles/lolwr.js
+++ commands/selfgameroles/lolwr.js
@@ -1,1 +1,33 @@
+const Discord = require("discord.js")
 
+module.exports= {
+    config: {
+        name: "lolwr",
+        description: "Adds the role Wild Rift to a yourself!",
+        usage: "^2lolwr",
+        category: "selfgameroles",
+        accessableby: "Members",
+        aliases: ["wildrift"]
+    },
+    run: async (bot, message, args) => {
+
+        message.delete().catch(O_o=>{});
+
+        let rMember = message.member
+        if(!rMember) return message.channel.send("An error occured")
+        let role = message.guild.roles.find(r => r.id == 791549009030152222)
+        if(!role) return message.channel.send("The role is not found on this server")
+
+        if(!message.guild.me.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"])) return message.channel.send("I don't have permission to perform this command.")
+
+        if(rMember.roles.has(role.id)) {
+            await rMember.removeRole(role.id).catch(e => console.log(e.message))
+            message.channel.send(`Role ${role.name} removed.`).then(msg => {msg.delete(5000)});
+        } else {
+            await rMember.addRole(role.id).catch(e => console.log(e.message))
+            message.channel.send(`Role ${role.name} added.`).then(msg => {msg.delete(5000)});
+
+        }
+
+    }
+}
