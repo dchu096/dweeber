46c97cdbc9c2bd4976d1f569c5f4eeebbd851dea
daniel chu <danielchu096@gmail.com>
2021-01-02 08:52:40 +1300

    update files
    nothing changed, just post update

--- commands/admins/message.js
+++ commands/admins/message.js
@@ -5,34 +5,35 @@ module.exports = {
     config: {
         name: "message",
         description: "message user in DM",
-        usage: "^2message <ID> <Message>",
+        usage: "^2message [ID] [Message]",
         category: "admins",
         accessableby: "admins",
-        aliases: ["send"]
+        aliases: ["sendmsg"]
     },
     run: async (bot, message, args) => {
         var embedColor = '#00FFFF' // color: cyan
 
-        let Owner = message.author;
         if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("You have no permission to process this command!");
 
-        const id = args.shift();
-        const sayMessage = args.join(" ")
-        if(!sayMessage) return message.reply("Usage `^2messgae <user>  <your message>`")
+        let Message = args.slice(1).join(' ') // .slice(1) removes the user mention, .join(' ') joins all the words in the message, instead of just sending 1 word
+        if(!Message) return message.reply("You havnt provide a message to be sent")
 
         let mentioned = message.mentions.members.first() || message.guild.members.get(args[0]); // Gets the user mentioned!
 
         let contact = new Discord.RichEmbed() //setup rich embed
-            .setAuthor(Owner.username)
-            .setColor("embedColor")
+            .setAuthor(message.author.username, message.author.avatarURL)
+            .setColor(embedColor)
             .setTitle(`A message from ${message.guild.name}`)
-            .addField("Response:", sayMessage)
+            .addField('From user:', message.author.tag)
+            .addField("Message:", Message)
+            .setFooter("Memubot | by dchu096")
             .setTimestamp()
 
         mentioned.send(contact);
 
+
         let chanemb = new Discord.RichEmbed()
-            .setColor("#00ff00")
+            .setColor(embedColor) // color: green
             .setDescription(`Message sent to ${mentioned.user.tag}`);
 
         message.channel.send(chanemb).then(msg => {msg.delete(5000)});
