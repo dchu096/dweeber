5d62a61257d556ebe469c5d6db3ca2f11b88cde1
daniel chu <danielchu096@gmail.com>
2021-01-02 08:53:16 +1300

    update files
    fixing some broken commands

--- commands/other/ping.js
+++ commands/other/ping.js
@@ -10,17 +10,15 @@ module.exports = {
     run: async (bot, message, args) => {
         var embedColor = '#87CEEB' // color: skyblue
 
-        message.channel.send("Pinging...").then(m => {
-            let ping = m.createdTimestamp - message.createdTimestamp
-            let choices = ["My ping details"]
-            let response = choices[Math.floor(Math.random() * choices.length)]
 
-        var Embed = new Discord.RichEmbed() // Creates the embed thats returned to the person warning if its sent.
-            .setColor(embedColor)
-            .setTitle("Ping")
-            .addField(`${response}: Bot Latency: \`${ping}\`, API Latency: \`${Math.round(bot.ping)}\``)
-
-            m.edit(Embed)
-    })
-  }
+        message.channel.send("Pinging...").then(msg => {
+            let ping = msg.createdTimestamp - message.createdTimestamp
+            let embed = new Discord.RichEmbed() //For discord v11 Change to new Discord.RichEmbed()
+                .setColor(embedColor)
+                .setTitle(`ping`)
+                .setDescription(`Bot Latency: \`${ping}\`, API Latency: \`${Math.round(bot.ping)}\``)
+            msg.edit(embed)
+        });
+    }
 }
+
