c476f9f13b6050ff93b9e258fd52e58be77b1825
daniel chu <danielchu096@gmail.com>
2021-01-18 08:48:40 +1300

    Add files via upload

--- commands/roles/addrole.js
+++ commands/roles/addrole.js
@@ -1,45 +1,80 @@
-const { RichEmbed } = require("discord.js")
-const { redlight } = require("../../colours.json")
+const Discord = require("discord.js")
 
 module.exports= {
     config: {
         name: "addrole",
         description: "Adds a role to a member of the guild!",
-        usage: "!addrole",
+        usage: "[user/ID]",
         category: "roles",
         accessableby: "Moderators",
         aliases: ["ar", "roleadd"]
     },
     run: async (bot, message, args) => {
 
-    if(!message.member.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"])) return message.channel.send("You dont have permission to perform this command!")
 
-    let rMember = message.mentions.members.first() || message.guild.members.find(m => m.user.tag === args[0]) || message.guild.members.get(args[0])
-    if(!rMember) return message.channel.send("Please provide a user to add a role too.")
-    let role = message.guild.roles.find(r => r.name == args[1]) || message.guild.roles.find(r => r.id == args[1]) || message.mentions.roles.first()
-    if(!role) return message.channel.send("Please provide a role to add to said user.") 
-    let reason = args.slice(2).join(" ")
-    if(!reason) return message.channel.send("Please provide a reason")
+        //messages
+        message.delete().catch(O_o => {});
 
-    if(!message.guild.me.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"])) return message.channel.send("I don't have permission to perform this command.")
+        const embedColor = '#87ceeb';
+        const warningColor = '#ff0000';
 
-    if(rMember.roles.has(role.id)) {
-        return message.channel.send(`${rMember.displayName}, already has the role!`)
-    } else {
-        await rMember.addRole(role.id).catch(e => console.log(e.message))
-        message.channel.send(`The role, ${role.name}, has been added to ${rMember.displayName}.`)
-    }
+        let rMember = message.mentions.members.first() || message.guild.members.cache.find(m => m.user.tag === args[0]) || message.guild.members.cache.get(args[0])
 
-    let embed = new RichEmbed()
-    .setColor(redlight)
-    .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
-    .addField("Moderation:", "Addrole")
-    .addField("Mutee:", rMember.user.username)
-    .addField("Moderator:", message.author.username)
-    .addField("Reason:", reason)
-    .addField("Date:", message.createdAt.toLocaleString())
-    
-        let sChannel = message.guild.channels.find(c => c.name === "tut-modlogs")
-        sChannel.send(embed)
-    }
+        let role = message.guild.roles.cache.find(r => r.name === args[1]) || message.guild.roles.cache.find(r => r.id === args[1]) || message.mentions.roles.first()
+
+        if (!rMember) {
+            let nopersonembed = new Discord.MessageEmbed()
+                .setTitle("❌Error")
+                .setDescription("Missing target user to add role")
+                .addField("required:", "mentions/ID", false)
+                .setColor(warningColor);
+            return message.channel.send(nopersonembed).then(msg => msg.delete({timeout: 5000})).catch(O_o => {
+            });
+        }
+
+        if (!role) {
+            let nopersonembed = new Discord.MessageEmbed()
+                .setTitle("❌Error")
+                .setDescription("Missing role to add on user")
+                .addField("required:", "role ID", false)
+                .setColor(warningColor);
+            return message.channel.send(nopersonembed).then(msg => msg.delete({timeout: 5000})).catch(O_o => {
+            });
+        }
+
+        if (!message.member.permissions.has("MANAGE_ROLES")) {
+            let nopermsembed = new Discord.MessageEmbed()
+                .setTitle("❌Error")
+                .setDescription("Missing MANAGE_ROLES permission (user)")
+                .addField("required:", "BAN_MEMBERS permission", false)
+                .setColor(warningColor);
+            return message.channel.send(nopermsembed).then(msg => msg.delete({timeout: 5000})).catch(O_o => {
+            });
+        }
+
+        if (!message.guild.me.permissions.has("MANGE_ROLES")) {
+            let botnopermsembed = new Discord.MessageEmbed()
+                .setTitle("❌Error")
+                .setDescription("Missing MANAGE_ROLES permission (bot)")
+                .addField("required:", "BAN_MEMBERS permission", false)
+                .setColor(warningColor);
+            return message.channel.send(botnopermsembed).then(msg => msg.delete({timeout: 5000})).catch(O_o => {
+            });
+        }
+
+        if(!rMember.roles.cache.has(role.id)) {
+            await rMember.roles.add(role.id).catch(O_o => {});
+
+            let roleadded = new Discord.MessageEmbed()
+                .setDescription(`Role ${role.name} added to ${rMember.displayName}`)
+                .setColor(embedColor);
+            await message.channel.send(roleadded).then(msg => msg.delete({timeout: 5000})).catch(O_o => {});
+        } else {
+            let alreadyhaveroleEmbed = new Discord.MessageEmbed()
+                .setTitle("❌Error")
+                .setDescription("This user already have the role")
+                .setColor(warningColor);
+            await message.channel.send(alreadyhaveroleEmbed).then(msg => msg.delete({timeout: 5000})).catch(O_o => {});
+        }
+}
 }
