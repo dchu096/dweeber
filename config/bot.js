1bffefa6b168cc7b5697ec1ef189d97728fdabf2
daniel chu <39256422+user096@users.noreply.github.com>
2021-05-22 13:47:33 +1200

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -1,1 +1,12 @@
+module.exports = {
+    emojis: {
+        off: '<a:ag_reddot:781410740619051008>',
+        error: '<a:ag_exc:781410611366985748>',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: '<a:ag_tickop:781395575962599445>',
+        loading: '<a:ag_loading:781410654841077780>'
+    },
 
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
