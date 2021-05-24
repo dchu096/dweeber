aa100528e57dca6e19b23349e8044b2c01445620
Sami Alameddine <68682150+alsamitech@users.noreply.github.com>
2021-05-23 22:01:29 -0700

    Replaced "is" with "has"

--- player/queueEnd.js
+++ player/queueEnd.js
@@ -5,7 +5,7 @@ module.exports = async (bot, message, queue) => {
     const stoppedEmbed = new Discord.MessageEmbed()
             .setColor('#0099ff')
     		.setTitle(`Playing in ${message.member.voice.channel.name}`)
-            .setDescription(`${message.client.emotes.error} - As theres no more music queued, the music is now stopped!`)
+            .setDescription(`${message.client.emotes.error} - As theres no more music queued, the music has now stopped!`)
 
     message.channel.send(stoppedEmbed);
 };
