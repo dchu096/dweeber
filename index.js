57fc1cfd39247eb9c6d99f21e42b90288673756b
Sami Alameddine <68682150+alsamitech@users.noreply.github.com>
2021-07-13 09:41:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ bot.on('guildMemberAdd', async member => {
     let welcomeEmbed = new Discord.MessageEmbed()
         .setTitle(`welcome ${member.user.tag} to MEmu Discord!`)
         .setDescription("Welcome to the MEmu community!")
-        .addField("Rules:", "We setup a server rules at <#790489245193732127> to maintain behaviour in server, please read so you dont break them!", false)
-        .addField("Roles:", " A lot of channels in this server requires a role to be assigned. get to <#786798319992307763> to get yourself some!", false)
+        .addField("Rules:", "We have our server rules at <#790489245193732127> to maintain behaviour in server, please read so you dont break them!", false)
+        .addField("Roles:", " A lot of channels in this server require a role to be assigned. get to <#786798319992307763> to get yourself some!", false)
         .addField("channels:", "channels have been setup to help easier. so please do post in the correct channels so we can troubleshoot faster!", false)
         .setFooter("As long as you follow, then welcome!")
 
@@ -63,7 +63,4 @@ bot.on("message", message => {
 });
 
 
-
-
 bot.login(token);
-
