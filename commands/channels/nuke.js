ed88c91154b0b8bead4b023f6b5e4f8d6e82fa42
Daniel Chu <39256422+dchu096@users.noreply.github.com>
2022-02-12 16:30:42 +1300

    Update nuke.js

--- commands/channels/nuke.js
+++ commands/channels/nuke.js
@@ -14,50 +14,51 @@ module.exports = {
 
     run: async (message) => {
 
- let nChannel = message.mentions.channels.first() || message.channel;
-        
+        let nChannel = message.mentions.channels.first() || message.channel;
+
         const embedColor = '#87CEEB'; // color: skyblue
-        
+
         const {Confirmation} = require('discord-interface');
 
-let replyMessage = message.reply(`Are you sure you want to nuke the channel ${nChannel}? **This operation cannot be undone!** Please think carefully. Reply with "yes" to confirm. Operation expires after 10s`);
+        let replyMessage = message.reply(`Are you sure you want to nuke the channel ${nChannel}? **This operation cannot be undone!** Please think carefully. Reply with "yes" to confirm. Operation expires after 10s`);
 
-let filter = message => message.author.id == message.author.id && message.content.toLowerCase() == 'yes';
-message.awaitMessages(filter, {max: 1, time: 20000}).then(collected => {
-    if (!replyMessage.deleted) replyMessage.delete();
-    if (!collected.first().deteled) collected.first().delete();
+        let filter = message => message.author.id == message.author.id && message.content.toLowerCase() == 'yes';
+        message.awaitMessages(filter, {max: 1, time: 20000}).then(collected => {
+            if (!replyMessage.deleted) replyMessage.delete();
+            if (!collected.first().deteled) collected.first().delete();
 
-confirmation.on('confirmation', confirmed => {
-    if (confirmed) {
+            confirmation.on('confirmation', confirmed => {
+                if (confirmed) {
 
-        let confirmedEmbed = new Discord.MessageEmbed()
-        .setColor(embedColor)
-        .setDescription(`${message.client.emotes.success} Thanks for confirming, the channel will be nuked in 3 seconds.`)
+                    let confirmedEmbed = new Discord.MessageEmbed()
+                        .setColor(embedColor)
+                        .setDescription(`${message.client.emotes.success} Thanks for confirming, the channel will be nuked in 3 seconds.`)
 
-                        message.reply(confirmedEmbed)
-                        message.channel.clone({
-                            parent: `${nChannel.parentID}`,
-                            position: message.channel.rawPosition
-                        }).then(channel => {
+                    message.reply(confirmedEmbed)
+                    message.channel.clone({
+                        parent: `${nChannel.parentID}`,
+                        position: message.channel.rawPosition
+                    }).then(channel => {
 
-                            let nukedEmbed = new Discord.MessageEmbed()
+                        let nukedEmbed = new Discord.MessageEmbed()
                             .setColor(embedColor)
                             .setDescription(`${message.client.emotes.success} Channel have been nuked`)
                             .setImage('https://i.gifer.com/6Ip.gif')
 
-                            channel.send(nukedEmbed)
-                        }).catch((err) => {
-                          message.reply(`${message.client.emotes.error} An error occured!` ).then(() => {
-console.log(err).then(logger.error(`An error occured in ${message.guild.name} using command nuke`))
-    });
-                            
+                        channel.send(nukedEmbed)
+                    }).catch((err) => {
+                        message.reply(`${message.client.emotes.error} An error occured!`).then(() => {
+                            console.log(err).then(logger.error(`An error occured in ${message.guild.name} using command nuke`))
                         });
 
-                        setTimeout(function () {
-                            msg.channel.delete();
-                        }, 3000);
-        
-            }
-});
-            }
+                    });
+
+                    setTimeout(function () {
+                        msg.channel.delete();
+                    }, 3000);
+
+                }
+            });
+        });
+    }
 }
