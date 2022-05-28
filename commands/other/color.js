2601847bacb449fcaf33236a4d3609659031870a
Daniel Chu <39256422+dchu096@users.noreply.github.com>
2022-05-29 10:43:13 +1200

    Update bot

--- commands/other/color.js
+++ commands/other/color.js
@@ -1,4 +1,7 @@
 const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const {Signale} = require('signale');
 module.exports = {
     config: {
         name: "color",
@@ -9,8 +12,33 @@ module.exports = {
         aliases: ["colors"]
     },
     run: async (bot, message, args) => {
+
+        if(!args[0]) {
+            message.channel.send("No color defined")
+        }
+
+        const embedColor = '#87CEEB'; // color: skyblue
+
+        await fetch(`https://api.popcat.xyz/color/${args[0].includes("#") ? args[0].split("#")[1] : args[0] }`).then(res => res.json()).then(json => {
+            
+            const responseEmbed = new Discord.MessageEmbed()
+            .setColor(embedColor)
+            .setTitle(`Color: ${json.name}`)
+            .setDescription(`Color info for ${json.name}`)
+            .addField(`Name`, `${json.name}`)
+            .addField(`Hex code`,`${json.hex}`)
+            .addField(`RGB`,`${json.rgb}`)
+            .addField(`brightened`,`${json.brightened}`)
+            .setImage(json.color_image)
+
+
+        message.channel.send(responseEmbed).catch((err) => {
+            signale.error(err)
+
+        });
+        });
+
        
-        
 
     }
 
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const { Client, Intents, Collection } = require("discord.js");
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const { token } = require("./botconfig.json");
+const { fs } = require("fs")
 const bot = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,  Intents.FLAGS.GUILD_PRESENCES]
   });
 
 const config = require('./botconfig.json');
 
-const fs = require('fs');
-
 ["aliases", "commands"].forEach(x => bot[x] = new Collection());
 ["console", "command", "event"].forEach(x => require(`./handlers/${x}`)(bot));
 
 bot.login(token);
-
