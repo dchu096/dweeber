b907bb63a91febe23ed761d28adf69a94a87f648
Daniel Chu <39256422+dchu096@users.noreply.github.com>
2022-05-28 23:26:33 +1200

    Update serverinfo.js

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -20,26 +20,15 @@ module.exports = {
             return days + (days === 1 ? " day" : " days") + " ago";
         }
 
-        let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
-
-        let region = {
-            "brazil": ":flag_br: Brazil",
-            "eu-central": ":flag_eu: Central Europe",
-            "singapore": ":flag_sg: Singapore",
-            "us-central": ":flag_us: U.S. Central",
-            "sydney": ":flag_au: Sydney",
-            "us-east": ":flag_us: U.S. East",
-            "us-south": ":flag_us: U.S. South",
-            "us-west": ":flag_us: U.S. West",
-            "eu-west": ":flag_eu: Western Europe",
-            "vip-us-east": ":flag_us: VIP U.S. East",
-            "london": ":flag_gb: London",
-            "amsterdam": ":flag_nl: Amsterdam",
-            "hongkong": ":flag_hk: Hong Kong",
-            "russia": ":flag_ru: Russia",
-            "southafrica": ":flag_za:  South Africa"
+        let VerificationLevels = {
+            NONE: 'None',
+            LOW: 'Low',
+            MEDIUM: 'Medium',
+            HIGH: '(╯°□°）╯︵ ┻━┻',
+            VERY_HIGH: '┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻'
         };
 
+        const owner = await message.guild.fetchOwner();
 
 
         const ServerInfoEmbed = new Discord.MessageEmbed() // Creates the embed thats returned to the person warning if its sent
@@ -47,18 +36,19 @@ module.exports = {
             .setTitle("Server Info")
             .setAuthor(`${message.guild.name}`, message.guild.iconURL({ format: 'png', dynamic: true, size: 1024 }))
             .addField(`Server Name:`, `${message.guild.name}`, true)
-            .addField(`Server Name:`, `${message.guild.id}`, true)
-            .addField(`Server Owner:`, `${message.guild.owner}`, true)
+            .addField(`Server ID:`, `${message.guild.id}`, true)
+            .addField(`Server Owner:`, `${owner.user.tag}`, true)
             .addField("Member Count:", `${message.guild.memberCount}`, true)
             .addField("Role Count:", `${message.guild.roles.cache.size}`, true)
             .addField("server ID", message.guild.id, true)
-            .addField("Region", region[message.guild.region], true)
             .addField("Total | Humans | Bots", `${message.guild.members.cache.size} | ${message.guild.members.cache.filter(member => !member.user.bot).size} | ${message.guild.members.cache.filter(member => member.user.bot).size}`, true)
-            .addField("Verification Level", verifLevels[message.guild.verificationLevel], true)
-            .addField("Channels", message.guild.channels.cache.size, true)
-            .addField("Roles", message.guild.roles.cache.size, true)
+            .addField("Total | Text | Voice", `${message.guild.channels.cache.size} | ${message.guild.channels.cache.filter(channel => channel.type == "GUILD_TEXT").size} | ${message.guild.channels.cache.filter(channel => channel.type == "GUILD_VOICE").size}`, true)
+            .addField("Verification Level", `${VerificationLevels[message.guild.verificationLevel]}`, true)
+            .addField("Channels", `${message.guild.channels.cache.size}`, true)
+            .addField("Roles", `${message.guild.roles.cache.size}`, true)
             .addField("Creation Date", `${message.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(message.channel.guild.createdAt)})`, true)
             .setThumbnail(message.guild.iconURL({ format: 'png', dynamic: true, size: 1024 }))
+            .setFooter({ text: 'Dweeber >> Serverinfo'});
 
         await message.channel.send({ embeds: [ServerInfoEmbed] });
     }
