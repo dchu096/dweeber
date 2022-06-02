25abf076ec7e4812bf44aa97621d75e55447d846
Daniel Chu <39256422+dchu096@users.noreply.github.com>
2022-06-02 22:38:30 +1200

    Update iss_position.js

--- commands/other/iss_position.js
+++ commands/other/iss_position.js
@@ -16,22 +16,25 @@ module.exports = {
         const embedColor = '#87CEEB'; // color: skyblue
         const signale = new Signale();
 
+        try {
+
+        
+
         await fetch(`http://api.open-notify.org/iss-now.json`).then(res => res.json()).then(json => {
             
-            const issEmbed = new Discord.MessageEmbed()
-            .setColor(embedColor)
-            .setTitle(`ISS Position`)
-            .setDescription(`${json.latitude}`, `${json.longitude}`)
-            .setFooter({ text: 'Dweeber >> iss_position'});
-
-
-        message.channel.send({ embeds: [issEmbed] }).catch((err) => {
-            signale.error(err)
-
-        });
+                const issEmbed = new Discord.MessageEmbed()
+                .setColor(embedColor)
+                .setTitle(`Internet space station current location`)
+                .setDescription(`Longitude: ${json.iss_position.longitude}, Latitude: ${json.iss_position.latitude}`)
+                .setFooter({ text: 'Dweeber >> iss_position'});
+    
+    
+            message.channel.send({ embeds: [issEmbed] })
         });
 
-       
+    } catch (err) {
+        signale.error(err);
+    }
 
     }
 
