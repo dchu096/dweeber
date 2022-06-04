2fa46b45662048fbb6dfd170bbff23c53110fe75
Daniel Chu <39256422+dchu096@users.noreply.github.com>
2022-06-05 10:33:56 +1200

    Add files via upload

--- commands/game/fortnite.js
+++ commands/game/fortnite.js
@@ -1,1 +1,36 @@
+require('module-alias/register')
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const {Signale} = require('signale');
+const { trackerAPI } = require("@root/botconfig.json")
+module.exports = {
+    config: {
+        name: "fortnite",
+        description: "Fetch fortnite stats from a user",
+        usage: "",
+        category: "game",
+        accessableby: "Members",
+    },
+    run: async (bot, message, args) => {
 
+        const embedColor = '#87CEEB'; // color: skyblue
+        const signale = new Signale();
+
+        const platform = args[0];
+        const epic = args[1];
+
+
+         await fetch(`https://api.fortnitetracker.com/v1/profile/${platform}/${epic}`,{
+        method: 'GET',
+        headers: {'TRN-Api-Key': `${trackerAPI}`}
+        }).then(res => res.json()).then(json => {
+            console.log(json)
+            message.channel.send("Information have been logged to console!")
+        });
+
+       
+
+    }
+
+}
