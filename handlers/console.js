236b961bb507ed991a9666ba5e17f1cbc952da35
Daniel Chu <39256422+dchu096@users.noreply.github.com>
2022-06-13 12:18:26 +1200

    Delete console.js

--- handlers/console.js
+++
@@ -1,7 +0,0 @@
-module.exports = (bot) => {
-let prompt = process.openStdin()
-prompt.addListener("data", res => {
-    let x = res.toString().trim().split(/ +/g)
-        bot.channels.get("555039958121971736").send(x.join(" "));
-    });
-}
