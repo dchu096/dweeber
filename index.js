44f1432b9de8e640a6b1d760d2c738b62bb045c2
Daniel Chu <39256422+dchu096@users.noreply.github.com>
2022-06-13 12:20:17 +1200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const { token } = require("./botconfig.json");
-
-const manager = new ShardingManager('./bot.js', { token: `${token}` });
-
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-
-manager.spawn();
