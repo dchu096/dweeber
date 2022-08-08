0e76ed4654211ffbea454a02a5f4ccb32bbdd691
Daniel Chu <39256422+dchu096@users.noreply.github.com>
2022-08-08 15:10:41 +1200

    Add files via upload

--- Schema/ModerationSchema.js
+++ Schema/ModerationSchema.js
@@ -3,9 +3,6 @@ const mongoose = require('mongoose')
 let Schema = new mongoose.Schema({
     userID: { type: String, require: true, unique: true },
     guildID: { type: String, require: true },
-    kicks: { type: Number, default: 0 },
-    bans: { type: Number, default: 0 },
-    warns: { type: Number, default: 0 },
     warnings: { type: Number, default: 0 },
     kicked: { type: Number, default: 0 },
     banned: { type: Number, default: 0 },
